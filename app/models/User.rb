47ee1b9c79143ffc25cda33f19f8fe08a5f31d1a
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2022-05-01 19:25:27 -0400

    Add one to many association between users and articles

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :articles
 	validates :username, presence: true, 
 			uniqueness: { case_sensitive: false }, 
 			length: { minimum: 3, maximum: 25 }
@@ -6,5 +7,5 @@ class User < ApplicationRecord
 	validates :email, presence: true, 
 			uniqueness: { case_sensitive: false }, 
 			length: { maximum: 105 },
-			format: {width: VALID_EMAIL_REGEX}
+			format: {with: VALID_EMAIL_REGEX}
 end
