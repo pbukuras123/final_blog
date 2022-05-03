16655ceb1b8ac467d660b848c3f1daaeaa7b77fd
pbukuras123 <59669536+pbukuras123@users.noreply.github.com>
2022-05-03 17:40:28 -0400

    Restrict user and article actions with before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:show, :edit, :update]
+before_action :require_user, only: [:edit, :update]
+before_action :require_same_user, only: [:edit, :update]
 
 	def show
-		@user = User.find(params[:id])
 		@articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
@@ -14,11 +16,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update(user_params)
 			flash[:notice] = "Your account information was successfully updated"
 			redirect_to @user
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:notice] = "Welcome to the final blog #{@user.username}, have successfully signed up"
+			session[:user_id] = @user.id
+			flash[:notice] = "Welcome to the final blog #{@user.username}, you have successfully signed up"
 			redirect_to articles_path
 		else
 			render 'new'
@@ -41,6 +42,20 @@ class UsersController < ApplicationController
 
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
+	end
+
+
+	private
+
+	def set_user
+		@user = User.find(params[:id])
+	end
+
+	def require_same_user
+		if current_user != @user 
+			flash[:alert] = "You can only edit your own profile"
+			redirect_to @user
+		end
 	end
 
 end
